acc2231e5fb64128793cc0d3b7f44ee2b6a518d7
mayank160903 <97187052+mayank160903@users.noreply.github.com>
2024-07-23 12:07:39 +0530

    initiating

--- src/components/News.js
+++ src/components/News.js
@@ -1,105 +1,209 @@
-import React from "react";
+// import React from "react";
+// import NewsItem from "./NewsItem";
+// import Spinner from "./Spinner";
+// import PropTypes from "prop-types";
+// import { useEffect, useState } from "react";
+// import InfiniteScroll from "react-infinite-scroll-component";
+
+
+// const News = (props) => {
+
+//   const [articles, setArticles] = useState([]);
+//   const [loading, setLoading] = useState(true);
+//   const [page, setPage] = useState(1);
+//   const [totalResults, setTotalResults] = useState(0);
+//   // const [country, setCountry] = useState(`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}&language=${props.language}`);
+  
+  
+
+//   const capitalizeFirstLetter = (string) => {
+//     return string.charAt(0).toUpperCase() + string.slice(1);
+//   };
+
+//   const handleCountryCode = ( ) => {
+    
+//   }
+
+//   const updateNews = async () => {
+//     props.setProgress(10);
+//     const url = `https://newsapi.org/v2/top-headlines?country=in&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+//     setLoading(true);
+//     let data = await fetch(url);
+//     props.setProgress(30);
+//     let parsedData = await data.json();
+//     props.setProgress(70);
+//     setArticles(parsedData.articles);
+//     setTotalResults(parsedData.totalResults);
+//     setLoading(false);
+//     props.setProgress(100);
+//   };
+
+//   useEffect(() => {
+//     document.title = `${capitalizeFirstLetter(
+//     props.category
+//   )} - NewsMonkey`;
+//     updateNews();
+    
+// // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [])
+
+
+//   const fetchMoreData = async () => {
+//     const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`;
+//     setPage(page+1);
+//     let data = await fetch(url);
+//     let parsedData = await data.json();
+//     setArticles(articles.concat(parsedData.articles));
+//     setTotalResults(parsedData.totalResults);
+//   };
+
+
+
+//     return (
+//       <>
+//         <h1 className="text-center">
+//           Top {capitalizeFirstLetter(props.category)} Headlines
+//         </h1>
+//         {loading && <Spinner />}
+//         <InfiniteScroll
+//           dataLength={articles.length}
+//           next={fetchMoreData}
+//           hasMore={articles.length !== totalResults}
+//           loader={<Spinner />}
+//           >
+//           <div className="container">
+            
+//           <div className="row">
+//             {articles.map((element) => {
+//               return (
+//                 <div className="col-md-4" key={element.url}>
+//                   <NewsItem
+//                     title={element.title ? element.title : ""}
+//                     description={element.description ? element.description : ""}
+//                     imageUrl={
+//                       element.urlToImage
+//                         ? element.urlToImage
+//                         : "https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg"
+//                     }
+//                     newsUrl={element.url}
+//                     author={element.author}
+//                     date={element.publishedAt}
+//                     source={element.source.name}
+//                   />
+//                 </div>
+//               );
+//             })}
+//           </div>
+//           </div>
+//         </InfiniteScroll>
+//       </>
+//     );
+//   }
+
+
+// News.defaultProps = {
+//   country: "in",
+//   pageSize: 8,
+//   category: "general",
+// };
+
+// News.propTypes = {
+//   country: PropTypes.string,
+//   pageSize: PropTypes.number,
+//   category: PropTypes.string,
+// };
+
+// export default News;
+
+
+import React, { useEffect, useState } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
-import { useEffect, useState } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 
-
 const News = (props) => {
-
   const [articles, setArticles] = useState([]);
   const [loading, setLoading] = useState(true);
   const [page, setPage] = useState(1);
   const [totalResults, setTotalResults] = useState(0);
-  // const [country, setCountry] = useState(`https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}&language=${props.language}`);
-  
-  
 
   const capitalizeFirstLetter = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
   };
 
-  const handleCountryCode = ( ) => {
-    
-  }
-
   const updateNews = async () => {
     props.setProgress(10);
-    const url = `https://newsapi.org/v2/top-headlines?country=in&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
     setLoading(true);
-    let data = await fetch(url);
-    props.setProgress(30);
-    let parsedData = await data.json();
-    props.setProgress(70);
-    setArticles(parsedData.articles);
-    setTotalResults(parsedData.totalResults);
+    try {
+      let data = await fetch(url);
+      props.setProgress(30);
+      let parsedData = await data.json();
+      props.setProgress(70);
+      setArticles(parsedData.articles || []);
+      setTotalResults(parsedData.totalResults || 0);
+    } catch (error) {
+      console.error("Failed to fetch news articles:", error);
+    }
     setLoading(false);
     props.setProgress(100);
   };
 
   useEffect(() => {
-    document.title = `${capitalizeFirstLetter(
-    props.category
-  )} - NewsMonkey`;
+    document.title = `${capitalizeFirstLetter(props.category)} - NewsMonkey`;
     updateNews();
-    
-// eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
-
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   const fetchMoreData = async () => {
-    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page+1}&pageSize=${props.pageSize}`;
-    setPage(page+1);
-    let data = await fetch(url);
-    let parsedData = await data.json();
-    setArticles(articles.concat(parsedData.articles));
-    setTotalResults(parsedData.totalResults);
+    const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page + 1}&pageSize=${props.pageSize}`;
+    setPage(page + 1);
+    try {
+      let data = await fetch(url);
+      let parsedData = await data.json();
+      setArticles((prevArticles) => prevArticles.concat(parsedData.articles || []));
+      setTotalResults(parsedData.totalResults || 0);
+    } catch (error) {
+      console.error("Failed to fetch more news articles:", error);
+    }
   };
 
-
-
-    return (
-      <>
-        <h1 className="text-center">
-          Top {capitalizeFirstLetter(props.category)} Headlines
-        </h1>
-        {loading && <Spinner />}
-        <InfiniteScroll
-          dataLength={articles.length > 0 ? articles.length : 0}
-          next={fetchMoreData}
-          hasMore={articles.length !== totalResults}
-          loader={<Spinner />}
-          >
-          <div className="container">
-            
+  return (
+    <>
+      <h1 className="text-center">Top {capitalizeFirstLetter(props.category)} Headlines</h1>
+      {loading && <Spinner />}
+      <InfiniteScroll
+        dataLength={articles.length}
+        next={fetchMoreData}
+        hasMore={articles.length !== totalResults}
+        loader={<Spinner />}
+      >
+        <div className="container">
           <div className="row">
-            {articles.map((element) => {
-              return (
-                <div className="col-md-4" key={element.url}>
-                  <NewsItem
-                    title={element.title ? element.title : ""}
-                    description={element.description ? element.description : ""}
-                    imageUrl={
-                      element.urlToImage
-                        ? element.urlToImage
-                        : "https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg"
-                    }
-                    newsUrl={element.url}
-                    author={element.author}
-                    date={element.publishedAt}
-                    source={element.source.name}
-                  />
-                </div>
-              );
-            })}
+            {articles.map((element) => (
+              <div className="col-md-4" key={element.url}>
+                <NewsItem
+                  title={element.title ? element.title : ""}
+                  description={element.description ? element.description : ""}
+                  imageUrl={
+                    element.urlToImage
+                      ? element.urlToImage
+                      : "https://thumbs.dreamstime.com/b/news-newspapers-folded-stacked-word-wooden-block-puzzle-dice-concept-newspaper-media-press-release-42301371.jpg"
+                  }
+                  newsUrl={element.url}
+                  author={element.author}
+                  date={element.publishedAt}
+                  source={element.source.name}
+                />
+              </div>
+            ))}
           </div>
-          </div>
-        </InfiniteScroll>
-      </>
-    );
-  }
-
+        </div>
+      </InfiniteScroll>
+    </>
+  );
+};
 
 News.defaultProps = {
   country: "in",
@@ -111,6 +215,8 @@ News.propTypes = {
   country: PropTypes.string,
   pageSize: PropTypes.number,
   category: PropTypes.string,
+  apiKey: PropTypes.string.isRequired,
+  setProgress: PropTypes.func.isRequired,
 };
 
 export default News;
